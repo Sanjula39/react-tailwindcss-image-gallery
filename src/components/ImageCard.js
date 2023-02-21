edcbee1458f188170a4565c1f78ca74268aaf0aa
Sanjula Ranasinghe <sanjularanasinghe39@gmail.com>
2023-02-21 21:39:50 +0530

    Add search

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 
 const ImageCard = ({ image }) => {
+    
 
     const tags = image.tags.split(',');
     return (
+        
+        
         <div className="max-w-sm rounded overflow-hidden shadow-lg " >
+            
             <img
                 src={image.webformatURL} alt="" className="w-full" />
             <div className="px-6 py-4">
@@ -37,6 +41,8 @@ const ImageCard = ({ image }) => {
                 ))}
             </div>
         </div>
+        
+        
     )
 }
 
